745674e470389b3235dd2457db71c02eda4a525c
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-03-13 17:31:25 -0500

    Removed build directory

--- build/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinay/Projects/hashmapcpp/lib/googletest
-# Build directory: /home/vinay/Projects/hashmapcpp/build/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
